0c5178185e73df60df911c42ea18e2cc7e2bbd76
Haris <haris_ackar@hotmail.com>
2018-07-26 12:10:45 +0200

    fine tuning adjusted

--- OCXO_App/OCXO_App/FineTuning.cs
+++ OCXO_App/OCXO_App/FineTuning.cs
@@ -8,23 +8,25 @@ namespace OCXO_App
     public class FineTuning
     {
         int nCounter = 0;
+        double oldPhase = 0;
 
         public TuningResult tune(double lastDAC, double lastPhase)
         {
             nCounter ++;
-            if (nCounter == 50) {
+            if (nCounter == 100) {
                 nCounter = 0;
 
                 if (lastPhase < 0)
                 {
-                    lastDAC--;
+                    if(oldPhase < Math.Abs(lastPhase)) { lastDAC--; }
                     //return new TuningResult(lastDAC - 1, TuningResult.Result.NOT_FINISHED);  // aging (+1)
                 }
                 else
                 {
-                    lastDAC++;
+                    if (oldPhase < Math.Abs(lastPhase)) { lastDAC++; }
                     //return new TuningResult(lastDAC + 1, TuningResult.Result.NOT_FINISHED);
                 }
+                oldPhase = Math.Abs(lastPhase);
             }
             return new TuningResult(lastDAC, TuningResult.Result.NOT_FINISHED);
         }
