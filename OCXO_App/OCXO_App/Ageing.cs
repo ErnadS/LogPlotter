f3a23b8f6eaef712b2e359008f9190210cd9e26a
Haris <haris_ackar@hotmail.com>
2018-07-27 07:54:08 +0200

    ageing

---
+++ OCXO_App/OCXO_App/Ageing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCXO_App
+{
+    public class Ageing
+    {
+        int nCounter = 0;
+        double oldPhase = 0;
+        public TuningResult ageing(double lastDAC, double lastPhase)
+        {
+            nCounter++;
+            if(nCounter == 1000)
+            {
+                nCounter = 0;
+                if (lastPhase < 0)
+                {
+                    if (oldPhase < Math.Abs(lastPhase)) { lastDAC--; }
+                    //return new TuningResult(lastDAC - 1, TuningResult.Result.NOT_FINISHED);  // aging (+1)
+                }
+                else
+                {
+                    if (oldPhase < Math.Abs(lastPhase)) { lastDAC++; }
+                    //return new TuningResult(lastDAC + 1, TuningResult.Result.NOT_FINISHED);
+                }
+                oldPhase = Math.Abs(lastPhase);
+            }
+            return new TuningResult(lastDAC, TuningResult.Result.NOT_FINISHED);
+        }
+    }
+}
