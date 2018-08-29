26742e842aecb5191e9ba7f4544b2168f61bce5d
Ernad Secic <ernad@secic.no>
2018-08-30 00:27:00 +0200

    - sliding frame vise ne korsiti exponential nego obicnu sredinju vrijednost - uveden "zeroCrossingTime" racunanje (nije provjereno). Ako ubrzo prolazi nulu, prekidamo SlidingFrame2 (ne pravimo step) nego stavljamo na optimalnu

---
+++ OCXO_App/OCXO_App/AverageNormal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OCXO_App
+{
+    class AverageNormal
+    {
+        public static double calculateExpAvgFromArray(double[] dInputs, int nStartInd, int length)
+        {
+            double output = 0;
+
+            for (int i = nStartInd; i < nStartInd + length; i++) 
+            {
+                output += dInputs[i];
+            }
+            return output / length;
+        }
+    }
+}
